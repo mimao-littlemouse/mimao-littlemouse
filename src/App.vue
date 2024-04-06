3b9014a09e11207b36aa7dbe5646a940ed7ac727
mimao-littlemouse <mimao_littlemouse@163.com>
2024-04-06 08:25:59 +0800

    搭建项目 主体 ，删除没必要的文件

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Layout from './components/Layout.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Layout>
+    <template #header="{ msg  }">
+      <div class="w-100 h-100 bg-info">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      </div>
+    </template>
+    <template #default>
+      <div class="w-100 h-100 bg-danger">
 
-  <main>
-    <TheWelcome />
-  </main>
+      </div>
+    </template>
+  </Layout>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-100 h-100">
+        <header class="w-100" style="height: 60px;">
+            <slot name="header" :msg="12"></slot>
+        </header>
+        <main class="w-100" style="height: calc(100% - 60px);">
+            <slot></slot>
+        </main>
+    </div>
+</template>
